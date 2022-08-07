ad1892386b3f35276bf6afbeb816b5371921f223
akram-webdev <akram.webdev@gmail.com>
2022-08-08 00:10:49 +0600

    weight and height adjustable for any screen

--- src/Components/Calculator/Calculator.js
+++ src/Components/Calculator/Calculator.js
@@ -1,17 +1,30 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { BUTTONS } from './BtnConfig';
 import './Calculator.css';
 
 const Calculator = () => {
+
+    const btnsRef = useRef(null);
+    const expRef = useRef(null);
+
+    useEffect(() => {
+        const btns = Array.from(btnsRef.current.querySelectorAll('button'));
+
+        btns.forEach(e => e.style.height = e.offsetWidth + 'px')
+     
+    }, [])
+    
+
+
     return (
         <div className='calculator'>
             <div className='calculator__result'>
-                <div className="calculator__result__exp">
+                <div ref={expRef} className="calculator__result__exp">
                     11212122
                 </div>
                 <div className="calculator__result__exp"></div>
             </div>
-            <div className="calculator__btns">
+            <div ref={btnsRef} className="calculator__btns">
                 {
                     BUTTONS.map((item, index) =>(
                         <button key={index} className={item.class}>
