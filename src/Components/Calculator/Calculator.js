924f25036ccd27520dc20c02280f7f9eb2c22ac0
akram-webdev <akram.webdev@gmail.com>
2022-08-07 16:15:27 +0600

    display all buttons on screen

--- src/Components/Calculator/Calculator.js
+++ src/Components/Calculator/Calculator.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import { BUTTONS } from './BtnConfig';
 import './Calculator.css';
 
 const Calculator = () => {
     return (
-        <div>
-            <h1>hello im The calculator</h1>
+        <div className='calculator'>
+            <div className='calculator__result'>
+                <div className="calculator__result__exp"></div>
+            </div>
+            <div className="calculator__btns">
+                {
+                    BUTTONS.map((item, index) =>(
+                        <button key={index} className={item.class}>
+                            {item.display}
+
+                        </button>
+                    ))
+                }
+            </div>
+
+           
         </div>
     );
 };
