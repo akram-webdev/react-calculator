db7d29165bbcfeb718aa67b44e8333c15f375b03
akram-webdev <akram.webdev@gmail.com>
2022-08-08 11:58:28 +0600

    typing animation added

--- src/Components/Calculator/Calculator.js
+++ src/Components/Calculator/Calculator.js
@@ -11,36 +11,60 @@ const Calculator = () => {
         const btns = Array.from(btnsRef.current.querySelectorAll('button'));
 
         btns.forEach(e => e.style.height = e.offsetWidth + 'px')
-     
+
     }, [])
 
-    const btnClick = (item) =>{
+    const btnClick = (item) => {
         console.log(item);
 
         const expDiv = expRef.current;
 
         if (item.action === BTN_ACTIONS.THEME) document.body.classList.toggle('dark');
+
+        if (item.action === BTN_ACTIONS.ADD) {
+            addAdminSpan(item.display);
+        }
     }
-    
+
+    const addAdminSpan = (content) => {
+        const expDiv = expRef.current;
+        const span = document.createElement('span');
+
+        span.innerHTML = content;
+        span.style.opacity = 0;
+        expDiv.appendChild(span)
+
+        const width = span.offsetWidth + 'px';
+        span.style.width = '0'
+
+        expDiv.parentNode.querySelector('div:last-child').style.transform = `
+           translateY(${-expDiv.offsetHeight + 'px'}) scale(0.4)
+        
+        `;
+
+        setTimeout(() => {
+            span.style.opacity = '1';
+            span.style.width = width
+        }, 100)
+    }
+
 
 
     return (
         <div className='calculator'>
             <div className='calculator__result'>
-                <div ref={expRef} className="calculator__result__exp">
-                    11212122
-                </div>
+                <div ref={expRef} className="calculator__result__exp"></div>
                 <div className="calculator__result__exp"></div>
             </div>
             <div ref={btnsRef} className="calculator__btns">
                 {
-                    BUTTONS.map((item, index) =>(
-                        <button 
-                           key={index}
-                           className={item.class}
-                           onClick = {() => btnClick(item)}
-                           >
-                           
+                    BUTTONS.map((item, index) => (
+                        <button
+                            key={index}
+                            className={item.class}
+                            onClick={() => btnClick(item)}
+                        >
+
 
 
                             {item.display}
@@ -49,7 +73,7 @@ const Calculator = () => {
                 }
             </div>
 
-           
+
         </div>
     );
 };
